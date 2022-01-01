5f64c0f584ba49047f8f7f25cdf615fc68d04a95
yassan-s <yasuro.2103@gmail.com>
2022-01-01 22:55:15 +0900

    ユーザー新規登録 エラーを修正 #2

--- src/main/java/com/example/application/controller/SecurityController.java
+++ src/main/java/com/example/application/controller/SecurityController.java
@@ -10,30 +10,54 @@ import com.example.application.entity.SiteUser;
 import com.example.application.form.SiteUserForm;
 import com.example.application.service.UserDetailsServiceImpl;
 
+/**
+* ログイン画面やユーザー登録を制御する
+*
+*/
 @Controller
 public class SecurityController {
 
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
 
+	/**
+	* ログイン画面
+	* @param
+	* @return login
+	*/
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 
+	/**
+	* ユーザー登録画面
+	* @param SiteUserForm siteUserForm
+	* @return signup
+	*/
 	@GetMapping("/signup")
-	public String signup() {
+	public String signup(@ModelAttribute SiteUserForm siteUserForm) {
 		return "signup";
 	}
 
+	/**
+	* ユーザー登録
+	* @param SiteUserForm siteUserForm
+	* @return redirect:/login
+	*/
 	@PostMapping("/signup")
 	public String register(@ModelAttribute SiteUserForm siteUserForm) {
 
 		userDetailsService.register(changeSiteUser(siteUserForm));
 
-		return "redeirct:/longin";
+		return "redirect:/login";
 	}
 
+	/**
+	* SiteUserForm(Form)からSiteUser(entity)に変換
+	* @param SiteUserForm siteUserForm
+	* @return siteUser
+	*/
 	public SiteUser changeSiteUser(SiteUserForm siteUserForm) {
 
 		SiteUser siteUser = new SiteUser();
