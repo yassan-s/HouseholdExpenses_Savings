f7461725e0599f4a6fb488aeda379c813882b32b
yassan-s <yasuro.2103@gmail.com>
2022-03-06 23:49:37 +0900

    分析画面の初期処理を実装中 #8

--- src/main/java/com/example/application/service/AccountBookSearchService.java
+++ src/main/java/com/example/application/service/AccountBookSearchService.java
@@ -1,9 +1,26 @@
 package com.example.application.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.application.entity.AccountBook;
+import com.example.application.mapper.AccountBookSearchMapper;
 
 @Service
 public class AccountBookSearchService {
 
+	@Autowired
+	private AccountBookSearchMapper SearchMapper;
 
+	/**
+	* 初期分析画面表示
+	* @param user_id
+	* @param thisMonth
+	* @return List<AccountBook>
+	*/
+	public List<AccountBook> getAccountBookList(int user_id){
+		return SearchMapper.getThisMonth(user_id);
+	}
 }
